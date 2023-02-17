644d4e3faa395f1ab47feab0090e467746ab21c1
Callum Snowden <10308126+callumsnowden@users.noreply.github.com>
2023-02-17 15:21:06 +0000

    Update NO2 sensor code
    Updated NO2 interface with latest calculation from SPEC sensors. Added moving average to attempt to smooth out readings. Bumped package version to 23.2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name = 'DesignSpark.ESDK',
     namespace_packages=['DesignSpark'],
     packages = ['DesignSpark.ESDK'],
-    version = '22.7.0',
+    version = '23.2.0',
     description = 'DesignSpark ESDK support library',
     author = 'RS Components',
     author_email = 'maint@abopen.com',
